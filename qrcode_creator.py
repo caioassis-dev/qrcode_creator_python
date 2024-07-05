c849ebf96f810a368987c6efce6929eeea4548d5
Caio Assis <caioassis@Users-MacBook-Pro.local>
2024-07-05 19:37:16 -0300

    refact(estrutura_tkinter_funcao):adicionando funcao e estrutura completa do tkinter.

--- qrcode_creator.py
+++ qrcode_creator.py
@@ -4,8 +4,37 @@ import pyqrcode
 import png
 
 def create_qr_code():
-    pass
+    nomeQRCODE = nome_qrcode_entry.get()
+    url = url_entry.get()
+    if url:
+        qr_code = pyqrcode.create(url)
+        qr_code.png(f'qr_code_{nomeQRCODE}.png', scale=8)
+        messagebox.showinfo("QR Code Generator", "QR code criado com sucesso!")
+    else:
+        messagebox.showerror("QR Code Generator", "Por favor entre com a URL.")
 
 root = tk.Tk
+
+root.title("QR Code Generator")
+
+# Label para digitar o site
+url_label = tk.Label(root, text="Digite o site que você deseja criar o QRCODE:")
+url_label.pack(padx=10, pady=5)
+
+# Campo para digitar a URL
+url_entry = tk.Entry(root, width=50)
+url_entry.pack(padx=10, pady=5)
+
+# Label para digitar o nome do QR Code
+nome_qrcode = tk.Label(root, text="Qual nome que você deseja para o QRCODE?")
+nome_qrcode.pack(padx=10, pady=5)
+
+# Campo para digitar o nome do QR Code
+nome_qrcode_entry = tk.Entry(root, width=50)
+nome_qrcode_entry.pack(padx=10, pady=5)
+
+# Botão para criar o QR Code
+generate_button = tk.Button(root, text="Criar QR Code", command=create_qr_code)
+generate_button.pack(padx=10, pady=20)
 
 root.mainloop()
